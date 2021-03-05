3fb4bf0ec4a5524b4cc8fd77a699e015ef62ccab
sborys00 <16275683+sborys00@users.noreply.github.com>
2021-03-05 19:59:50 +0100

    add navbar and content styles

--- WhatToDo.Website/ClientApp/src/app/app.module.ts
+++ WhatToDo.Website/ClientApp/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { PlaceCardComponent } from './place-card/place-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    PlaceCardComponent
   ],
   imports: [
     BrowserModule,
