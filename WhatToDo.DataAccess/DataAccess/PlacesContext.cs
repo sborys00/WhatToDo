cff25baf62f32eafcfaacea5725b43e652be5f1c
sborys00 <16275683+sborys00@users.noreply.github.com>
2021-03-05 13:37:50 +0100

    add add places context

---
+++ WhatToDo.DataAccess/DataAccess/PlacesContext.cs
@@ -0,0 +1,22 @@
+﻿using WhatToDo.DataAccess.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.DataAccess
+{
+    public class PlacesContext : DbContext
+    {
+        public PlacesContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Place> Places { get; set; }
+        public DbSet<Adress> Adresses { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Image> Images { get; set; }
+        public DbSet<OpeningHours> OpeningHours { get; set; }
+        public DbSet<Url> Urls { get; set; }
+    }
+}
--- WhatToDo.Website/Startup.cs
+++ WhatToDo.Website/Startup.cs
@@ -1,7 +1,9 @@
+using DataAccess.DataAccess;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -21,6 +23,10 @@ namespace WhatToDo.Website
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<PlacesContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("DB"));
+            });
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
