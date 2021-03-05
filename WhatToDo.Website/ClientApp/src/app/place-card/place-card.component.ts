75eff70503097509a3c2b9a356a1f0a67269ed12
sborys00 <16275683+sborys00@users.noreply.github.com>
2021-03-05 21:44:52 +0100

    add place-card component

---
+++ WhatToDo.Website/ClientApp/src/app/place-card/place-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-place-card',
+  templateUrl: './place-card.component.html',
+  styleUrls: ['./place-card.component.css']
+})
+export class PlaceCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
