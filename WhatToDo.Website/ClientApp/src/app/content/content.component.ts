36974a4b3823cf66c2e11a61ccfee94e39fc75f1
sborys00 <16275683+sborys00@users.noreply.github.com>
2021-03-06 18:12:32 +0100

    add navigation buttons

--- WhatToDo.Website/ClientApp/src/app/content/content.component.ts
+++ WhatToDo.Website/ClientApp/src/app/content/content.component.ts
@@ -10,14 +10,28 @@ import { PlacesService } from '../shared/services/places.service';
 export class ContentComponent implements OnInit {
 
   places: Place[] = [];
-
+  place: Place = this.places[0];
+  i: number = 0;
   constructor(ps: PlacesService) {
     ps.getPlaces().subscribe(places => {
-       this.places = places;
+      this.places = places;
+      this.place = this.places[0];
     });
   }
 
   ngOnInit(): void {
   }
 
+  nextPlace(): void {
+    if (this.i < this.places.length - 1) {
+      this.i++;
+      this.place = this.places[this.i];
+    }
+  }
+  prevPlace(): void {
+    if (this.i > 0) {
+      this.i--;
+      this.place = this.places[this.i];
+    }
+  }
 }
