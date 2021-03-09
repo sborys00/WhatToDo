acd41ac77ac5463a49e574dd9b2aed8cadd2841c
sborys00 <16275683+sborys00@users.noreply.github.com>
2021-03-09 18:17:48 +0100

    add categories controller

---
+++ WhatToDo.Website/Controllers/CategoriesController.cs
@@ -0,0 +1,29 @@
+﻿using DataAccess.DataAccess;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WhatToDo.DataAccess.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace WhatToDo.Website.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly PlacesContext _db;
+
+        public CategoriesController(PlacesContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Category>> GetCategoriesAsync()
+        {
+            return await _db.Categories.ToListAsync();
+        }
+    }
+}
