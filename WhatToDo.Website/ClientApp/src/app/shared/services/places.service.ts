37881a97f350db01bcd3b0f098452e605c749e04
sborys00 <16275683+sborys00@users.noreply.github.com>
2021-03-09 18:12:56 +0100

    add categories to service

--- WhatToDo.Website/ClientApp/src/app/shared/services/places.service.ts
+++ WhatToDo.Website/ClientApp/src/app/shared/services/places.service.ts
@@ -2,17 +2,24 @@ import { Injectable } from "@angular/core";
 import { Observable } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Place } from "../models/place.model";
+import { Category } from '../models/category.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PlacesService {
-  apiUrl: string = "api/Places"
+  apiUrl: string = "api";
+  placesEndpoint: string = "places";
+  categoriesEndpoint: string = "categories";
   limit: number = 5;
   random: boolean = true;
 
   constructor(private http: HttpClient) { }
   getPlaces(limit: number = this.limit, random: boolean = this.random): Observable<Place[]> {
-    return this.http.get<Place[]>(`${this.apiUrl}?limit=${this.limit}&random=${this.random}`);
+    return this.http.get<Place[]>(`${this.apiUrl}/${this.placesEndpoint}?limit=${this.limit}&random=${this.random}`);
+  }
+
+  getCategories() {
+    return this.http.get<Category[]>(`${this.apiUrl}/${this.categoriesEndpoint}`);
   }
 }
