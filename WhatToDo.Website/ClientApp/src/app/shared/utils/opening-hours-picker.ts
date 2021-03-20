c8c6a3f3650646df88966bd817e804a750f3b730
Juliusz Zagdański <julek@zagdanski.pl>
2021-03-20 16:29:38 +0100

    fix wrongly displayed hours

--- WhatToDo.Website/ClientApp/src/app/shared/utils/opening-hours-picker.ts
+++ WhatToDo.Website/ClientApp/src/app/shared/utils/opening-hours-picker.ts
@@ -2,7 +2,7 @@ import { OpeningHours } from '../models/opening-hours.model';
 
 export class OpeningHoursPicker {
   static pickHours(hours: OpeningHours[]){
-    let day = new Date().getDay();
+    let day = (new Date().getDay() - 1) % 7;
     let todayHours = new OpeningHours();
 
     if (hours != undefined) {
